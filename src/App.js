62f8c2d133aa2fe086ca4c8cd48a2a816e7b0e55
Kalio007 <Kalioprince1@gmail.com>
2022-12-05 22:37:10 +0100

    body work upload

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Nav from './Components/Nav/Nav';
 import Header from "./Components/Header/Header";
 import Brand from './Components/Brand/Brand';
+import GPT3 from './Components/GPT3/GPT3';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Header/>
       </div>
       <Brand />
+      <GPT3/>
       </div>
     
   );
